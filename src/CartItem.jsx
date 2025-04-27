978bc2074a856725b6a2f318a46bfc10b3f509f7
Michael Avoyan <64565419+michaelavoyan@users.noreply.github.com>
2025-04-27 13:24:20 +0300

    cont

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,37 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+      let total = 0;
+        cart.forEach(item => {
+            const cost = parseFloat(item.cost.substring(1))
+            total += cost * item.quantity;
+        });
+        return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+      onContinueShopping(e);
   };
 
-
+    const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+    };
 
   const handleIncrement = (item) => {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+      dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+      dispatch(calculateTotalAmount(item));
   };
 
   return (
